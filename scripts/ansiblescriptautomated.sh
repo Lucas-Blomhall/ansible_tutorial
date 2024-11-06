a42af33a1ab435a18e23163eab3402e51d2c6217
Lucas Blomhäll <lucas.blomhall@gmail.com>
2024-11-06 15:44:30 +0100

    edited files

--- scripts/ansiblescriptautomated.sh
+++ scripts/ansiblescriptautomated.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Store the sudo password in a variable (be careful as it will be visible in the script)
-SUDO_PASS="Tennringen59"
+SUDO_PASS="old_solution"
 
 # Run each Ansible playbook with the sudo password directly provided
 ansible-playbook --extra-vars "ansible_become_password=$SUDO_PASS" /home/lucasb/ansible_tutorial/server1_install_apache.yml
